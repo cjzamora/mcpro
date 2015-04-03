1fea0b61b184e5e2737e503d3444f8f9ae84b289
Jeff Simons Decena <marq.cruz99@gmail.com>
2015-04-03 08:33:57 +0800

    Added the base url in the .htaccess to prevent too many redirects. Deleted also the admin controllers and routes since this does not have admin.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -14,17 +14,15 @@ class UsersTableSeeder extends Seeder {
 		\DB::table('users')->insert(array (
 			0 => 
 			array (
-				'id_user' => '4',
+				'id_user' => '1',
 				'firstname' => 'John',
 				'lastname' => 'Doe',
 				'email' => 'admin@admin.com',
-				'password' => '$2y$10$To9p5w5m6IMqiE7J7TkyNuIF/B4bjIaZUWuSsoP.FC9IkThWdWJCO',
+				'password' => Hash::make('Testing123'),
 				'status' => '1',
 				'id_role' => '1',
 				'remember_token' => NULL,
-				'password_reset' => '$2y$10$.sFCbVKn7/cRy0F3npNp9OGxifI5zb1dFFlBgNhYSQkWsRzWgIgUm',
-				'created_at' => '2014-11-05 10:19:49',
-				'updated_at' => '2014-11-05 10:19:49',
+				'password_reset' => Hash::make('ResetPassWord!!'),
 			),
 		));
 	}
